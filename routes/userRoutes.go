8a550952406817f3f196817d670282436133d5f5
Muyi2905 <obaremimuyiwa@gmail.com>
2024-10-02 10:11:00 +0100

    updt funcftion

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -13,7 +13,7 @@ func UserRoutes(r *gin.Engine, db *gorm.DB) {
 		r.GET("/:id", func(c *gin.Context) { controllers.GetUserById(c, db) })
 		r.POST("/", func(c *gin.Context) { controllers.CreateUser(c, db) })
 		r.PUT("/:id", func(c *gin.Context) { controllers.UpdateUser(c, db) })
-		r.DELETE("/:id", func(c *gin.Context) { controllers.Delete(c, db) })
+		r.DELETE("/:id", func(c *gin.Context) { controllers.DeleteUser(c, db) })
 		r.POST("/signup", func(c *gin.Context) { controllers.Signup(c, db) })
 		r.POST("/login", func(c *gin.Context) { controllers.Login }(c, db))
 	}
